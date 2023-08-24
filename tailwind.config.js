49a93f79c6ac297c087f4db618413ba58a25bf8c
AprioriDev <apriorifox.dev@gmail.com>
2023-08-24 13:50:26 +0600

    hollow commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "main": "#1fb6ff",
+      "secondary": "#",
+      "danger": "",
+      
+    },
     extend: {},
   },
   plugins: [],

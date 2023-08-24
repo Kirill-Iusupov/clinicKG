aa46a57bf58db0948dc3be2f5de9d81f459c20fe
AprioriDev <apriorifox.dev@gmail.com>
2023-08-24 13:10:37 +0600

    init commit

---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "antd"
+import MainPage from "../pages/mainPage"
+
+function App() {
+
+  return (
+    <>
+      <MainPage />
+      <Button />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/mainPage/ui/mainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainPage = () => {
+
+
+    return (
+        <div className='container'>
+            <h1>This is main page</h1>
+        </div>
+    );
+}
+
+export default MainPage;
